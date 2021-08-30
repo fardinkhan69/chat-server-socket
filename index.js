38163c2b77fa0d5abc511acccaac4ce22158e10a
fardinkhan69 <fardinofficial69@gmail.com>
2021-08-30 20:37:26 +0600

    done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: "https://gallant-snyder-c618fb.netlify.app/",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
